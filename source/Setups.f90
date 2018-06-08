9621b74f4eeb720e7b9b87206088cfd4aad2e15f
HATSwg <39804032+HATSwg@users.noreply.github.com>
2018-06-08 11:43:37 -0400

    Update Setups.f90

--- source/Setups.f90
+++ source/Setups.f90
@@ -40,12 +40,10 @@ Contains
 Subroutine Setup_HATS(prompt_for_exit,screen_progress,paths_files,n_neutron_histories,absolute_n_histories,setup_file)
     !Reads in problem data, method data, physics data
     !Initializes processes and variables
-    Use IFPORT, Only: $MAXPATH
-    Use IFPORT, Only: GETDRIVEDIRQQ
-    Use IFPORT, Only: FILE$CURDRIVE
     Use Kinds, Only: dp
+    Use FileIO_Utilities, Only: max_path_len
     Use FileIO_Utilities, Only: slash
-    Use FileIO_Utilities, Only: Get_Working_Directory
+    Use FileIO_Utilities, Only: Working_Directory
     Implicit None
     Logical, Intent(Out) :: prompt_for_exit
     Logical, Intent(Out) :: screen_progress
@@ -58,7 +56,7 @@ Subroutine Setup_HATS(prompt_for_exit,screen_progress,paths_files,n_neutron_hist
     Logical :: force_overwrite
     Integer :: setup_unit,stat,num_cmd_args
     Integer :: pathlen, status
-    Character($MAXPATH) :: path
+    Character(max_path_len) :: path
     Logical :: interim_outputs
     Character(4) :: interim_unit
     Integer :: interim_period
@@ -68,8 +66,8 @@ Subroutine Setup_HATS(prompt_for_exit,screen_progress,paths_files,n_neutron_hist
                                 & interim_outputs,interim_unit,interim_period
     
     Call paths_files%Initialize()
-    Allocate(Character($MAXPATH) :: output_folder)
-    Allocate(Character($MAXPATH) :: file_suffix)
+    Allocate(Character(max_path_len) :: output_folder)
+    Allocate(Character(max_path_len) :: file_suffix)
     !Get path and name of executable
     Call GET_COMMAND_ARGUMENT(0, path, pathlen, status)
     If (status .NE. 0) Then
@@ -78,7 +76,7 @@ Subroutine Setup_HATS(prompt_for_exit,screen_progress,paths_files,n_neutron_hist
     End If
     paths_files%program_exe = Trim(path)
     !default values for files and directories
-    Call Get_Working_Directory(path,slash)
+    Call Working_Directory(GETdir=path,slash)
     paths_files%setup_file = Trim(path)//'HATS_Setup.txt'
     paths_files%resources_directory = Trim(path)//'Resources'//slash
     paths_files%results_directory = Trim(path)//'Results'//slash
@@ -91,7 +89,7 @@ Subroutine Setup_HATS(prompt_for_exit,screen_progress,paths_files,n_neutron_hist
     End If
     Read(setup_unit,NML = ProgramSetupList)
     Close(setup_unit)
-    !DIR$ IF DEFINED (MIC)
+    !DIR$ IF DEFINED (LIN_OS)
         prompt_for_exit = .FALSE.
     !DIR$ END IF
     !Trim output folder and file suffix character strings
@@ -160,7 +158,7 @@ Subroutine Create_Output_File_names(dir,suff,log_name,TE_name,T_name,E_name,F_na
 End Subroutine Create_Output_File_names
 
 Subroutine Check_files_exist(overwrite,n_cmd_args,paths_files)
-    Use IFPORT, Only: $MAXPATH
+    Use FileIO_Utilities, Only: max_path_len
     Use FileIO_Utilities, Only: slash
     Implicit None
     Logical, Intent(In) :: overwrite
@@ -168,7 +166,7 @@ Subroutine Check_files_exist(overwrite,n_cmd_args,paths_files)
     Type(paths_files_type), Intent(InOut) :: paths_files
     Integer, Parameter :: n_names = 8
     Logical :: file_exists(1:n_names)
-    Character($MAXPATH) :: file_name(1:n_names)
+    Character(max_path_len) :: file_name(1:n_names)
     Character(64) :: user_inp
     Integer :: i
 
@@ -254,36 +252,20 @@ Subroutine Check_files_exist(overwrite,n_cmd_args,paths_files)
 End Subroutine Check_files_exist
 
 Subroutine Check_folders_exist(paths_files)
-    Use IFPORT, Only: MAKEDIRQQ
+    Use FileIO_Utilities, Only: Check_Directory
+    Use FileIO_Utilities, Only: Create_Directory
     Implicit None
     Type(paths_files_type), Intent(In) :: paths_files
-    Logical :: folder_exists
-    Logical :: success
     
     !Check if resources directories exist
-    INQUIRE(DIRECTORY = paths_files%resources_directory , EXIST = folder_exists)
-    If (.NOT. folder_exists) Then
+    If (.NOT. Check_Directory(paths_files%resources_directory)) Then
         Print *,'ERROR:  Setups: Setup_HATS:  Resources directory not found: '//paths_files%resources_directory
         ERROR STOP
     End If
     !Check if results directories exist
-    INQUIRE(DIRECTORY = paths_files%results_directory , EXIST = folder_exists)
-    If (.NOT. folder_exists) Then  !create results folder
-        success = MAKEDIRQQ(paths_files%results_directory)
-        If (.NOT. success) Then
-            Print *,'ERROR:  Setups: Setup_HATS:  Failed to create directory: '//paths_files%results_directory
-            ERROR STOP
-        End If
-    End If
+    If (.NOT. Check_Directory(paths_files%results_directory)) Call Create_Directory(paths_files%results_directory)
     If (paths_files%output_folder .NE. '')  Then !output folder is specified
-        INQUIRE(DIRECTORY = paths_files%results_directory//paths_files%output_folder , EXIST = folder_exists)
-        If (.NOT. folder_exists) Then  !create output folder
-            success = MAKEDIRQQ(paths_files%results_directory//paths_files%output_folder)
-            If (.NOT. success) Then
-                Print *,'ERROR:  Setups: Setup_HATS:  Failed to create directory: '//paths_files%results_directory//paths_files%output_folder
-                ERROR STOP
-            End If
-        End If
+        If (.NOT. Check_Directory(paths_files%results_directory)) Call Create_Directory(paths_files%results_directory//paths_files%output_folder)
     End if
 End Subroutine Check_folders_exist
 
@@ -316,11 +298,10 @@ End Subroutine Setup_Estimator
 
 !DIR$ IF DEFINED (COA)
 Subroutine Setup_Info_to_disk(n_histories,abs_n_histories,prompt_for_exit,screen_progress,paths_files)
-    Use IFPORT, Only: $MAXPATH
-    Use IFPORT, Only: MAKEDIRQQ
     Use Kinds, Only: dp
+    Use FileIO_Utilities, Only: max_path_len
     Use FileIO_Utilities, Only: slash
-    Use FileIO_Utilities, Only: Get_Working_Directory
+    Use FileIO_Utilities, Only: Working_Directory
     Use FileIO_Utilities, Only: Var_to_File
     Implicit None
     Integer(8), Intent(In) :: n_histories
@@ -329,22 +310,15 @@ Subroutine Setup_Info_to_disk(n_histories,abs_n_histories,prompt_for_exit,screen
     Logical, Intent(In) :: screen_progress
     Type(Paths_Files_Type), Intent(In) :: paths_files
     Logical :: folder_exists,success
-    Character($MAXPATH) :: dir
+    Character(max_path_len) :: dir
     Character(:), Allocatable :: file_name,file_dir
 
-    Call Get_Working_Directory(dir,slash)
-    Allocate(Character($MAXPATH) :: file_dir)
+    Call Working_Directory(GETdir=dir,slash)
+    Allocate(Character(max_path_len) :: file_dir)
     file_dir = Trim(dir)//'temp'//slash
     !Check if temp results directory exists
-    INQUIRE(DIRECTORY = file_dir , EXIST = folder_exists)
-    If (.NOT. folder_exists) Then  !create results folder
-        success = MAKEDIRQQ(file_dir)
-        If (.NOT. success) Then
-            Print *,'ERROR:  Setups: Setup_Info_to_disk:  Failed to create directory: '//file_dir
-            ERROR STOP
-        End If
-    End If
-    Allocate(Character($MAXPATH) :: file_name)
+    If (.NOT. Check_Directory(file_dir)) Call Create_Directory(file_dir)
+    Allocate(Character(max_path_len) :: file_name)
     !write n_histories to file
     file_name = file_dir//'n.tmp'
     Call Var_to_File(n_histories,file_name)
@@ -399,10 +373,10 @@ End Subroutine Setup_Info_to_disk
 
 !DIR$ IF DEFINED (COA)
 Subroutine Setup_Info_from_disk(n_histories,abs_n_histories,prompt_for_exit,screen_progress,paths_files)
-    Use IFPORT, Only: $MAXPATH
     Use Kinds, Only: dp
+    Use FileIO_Utilities, Only: max_path_len
     Use FileIO_Utilities, Only: slash
-    Use FileIO_Utilities, Only: Get_Working_Directory
+    Use FileIO_Utilities, Only: Working_Directory
     Use FileIO_Utilities, Only: Var_from_File
     Implicit None
     Integer(8), Intent(Out) :: n_histories
@@ -410,14 +384,14 @@ Subroutine Setup_Info_from_disk(n_histories,abs_n_histories,prompt_for_exit,scre
     Logical, Intent(Out) :: prompt_for_exit
     Logical, Intent(Out) :: screen_progress
     Type(Paths_Files_Type), Intent(InOut) :: paths_files
-    Character($MAXPATH) :: dir
+    Character(max_path_len) :: dir
     Character(:), Allocatable :: file_name,file_dir
     Character($MAXPATH) :: C_tmp
     
-    Call Get_Working_Directory(dir)
-    Allocate(Character($MAXPATH) :: file_dir)
-    file_dir = Trim(dir)//slash//'temp'//slash
-    Allocate(Character($MAXPATH) :: file_name)
+    Call Working_Directory(GETdir=dir,s=slash)
+    Allocate(Character(max_path_len) :: file_dir)
+    file_dir = Trim(dir)//'temp'//slash
+    Allocate(Character(max_path_len) :: file_name)
     !read n_histories from file
     file_name = file_dir//'n.tmp'
     Call Var_from_File(n_histories,file_name)
@@ -492,75 +466,69 @@ End Subroutine Setup_Info_from_disk
 
 !DIR$ IF DEFINED (COA)
 Subroutine Cleanup_temp_files()
-    Use IFPORT, Only: $MAXPATH
-    Use IFPORT, Only: DELFILESQQ
-    Use IFPORT, Only: DELDIRQQ
+    Use FileIO_Utilities, Only: max_path_len
     Use FileIO_Utilities, Only: slash
-    Use FileIO_Utilities, Only: Get_Working_Directory
+    Use FileIO_Utilities, Only: Working_Directory
+    Use FileIO_Utilities, Only: Delete_Directory
     Implicit None
-    Integer :: n_del
-    Logical :: success
-    Character($MAXPATH) :: dir
+    Character(max_path_len) :: dir
     
-    Call Get_Working_Directory(dir,slash)
-    !delete all files in the directory
-    n_del = DELFILESQQ(Trim(dir)//'temp'//slash//'*')
+    Call Working_Directory(GETdir=dir,s=slash)
     !delete the temp directory
-    success = DELDIRQQ(Trim(dir)//'temp'//slash)
+    Call Delete_Directory(Trim(dir)//'temp')
 End Subroutine Cleanup_temp_files
 !DIR$ END IF
 
 Subroutine Initialize_Paths_Files(paths_files)
-    Use IFPORT, Only: $MAXPATH
+    Use FileIO_Utilities, Only: max_path_len
     Implicit None
     Class(Paths_Files_Type), Intent(InOut) :: paths_files
     Character(9), Parameter :: empty_string = '<<EMPTY>>'
 
     !allocate character variables with an arbitrary length, each assignment statement then auto-reallocates them to the correct length
-    Allocate(Character($MAXPATH) :: paths_files%app_title)
+    Allocate(Character(max_path_len) :: paths_files%app_title)
     paths_files%app_title = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%program_exe)
+    Allocate(Character(max_path_len) :: paths_files%program_exe)
     paths_files%program_exe = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%setup_file)
+    Allocate(Character(max_path_len) :: paths_files%setup_file)
     paths_files%setup_file = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%resources_directory)
+    Allocate(Character(max_path_len) :: paths_files%resources_directory)
     paths_files%resources_directory = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%results_directory)
+    Allocate(Character(max_path_len) :: paths_files%results_directory)
     paths_files%results_directory = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%cs_setup_file)
+    Allocate(Character(max_path_len) :: paths_files%cs_setup_file)
     paths_files%cs_setup_file = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%output_folder)
+    Allocate(Character(max_path_len) :: paths_files%output_folder)
     paths_files%output_folder = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%file_suffix)
+    Allocate(Character(max_path_len) :: paths_files%file_suffix)
     paths_files%file_suffix = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%log_file_name)
+    Allocate(Character(max_path_len) :: paths_files%log_file_name)
     paths_files%log_file_name = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%run_file_name)
+    Allocate(Character(max_path_len) :: paths_files%run_file_name)
     paths_files%run_file_name = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%TE_file_name)
+    Allocate(Character(max_path_len) :: paths_files%TE_file_name)
     paths_files%TE_file_name = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%t_file_name)
+    Allocate(Character(max_path_len) :: paths_files%t_file_name)
     paths_files%t_file_name = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%E_file_name)
+    Allocate(Character(max_path_len) :: paths_files%E_file_name)
     paths_files%E_file_name = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%f_file_name)
+    Allocate(Character(max_path_len) :: paths_files%f_file_name)
     paths_files%f_file_name = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%d_file_name)
+    Allocate(Character(max_path_len) :: paths_files%d_file_name)
     paths_files%d_file_name = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%m_file_name)
+    Allocate(Character(max_path_len) :: paths_files%m_file_name)
     paths_files%m_file_name = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%o_file_name)
+    Allocate(Character(max_path_len) :: paths_files%o_file_name)
     paths_files%o_file_name = empty_string
-    Allocate(Character($MAXPATH) :: paths_files%s_file_name)
+    Allocate(Character(max_path_len) :: paths_files%s_file_name)
     paths_files%s_file_name = empty_string
 End Subroutine Initialize_Paths_Files
 
 Subroutine Write_Setup_Information(n_img,t_process,t_elapsed_min,t_elapsed_max,n_h_hit,n_h_run,RNG,paths_files,file_name)
-    Use IFPORT, Only: FDATE
-    Use IFPORT, Only: HOSTNAM,MAX_HOSTNAM_LENGTH
-    Use OMP_LIB, Only:  OMP_get_num_threads
     Use Kinds, Only: dp
     Use Random_Numbers, Only: RNG_Type
+    Use FileIO_Utilities, Only: Date_Time_string
+    Use FileIO_Utilities, Only: Get_Host_Name
     Implicit None
     Integer, Intent(In) :: n_img
     Real(dp), Intent(In) :: t_process,t_elapsed_min,t_elapsed_max
@@ -581,13 +549,12 @@ Subroutine Write_Setup_Information(n_img,t_process,t_elapsed_min,t_elapsed_max,n
     Write(unit,'(A)') '------------------------------------------------------------------------'
     Write(unit,'(A)') paths_files%app_title
     Write(unit,'(A)') '------------------------------------------------------------------------'
-    Write(unit,'(2A)') '  Run Complete: ',FDATE()
+    Write(unit,'(2A)') '  Run Complete: ',Date_Time_string()
     Write(unit,'(A,f11.3,A)') '  Total Compute Time: ',t_process,' sec'
     Write(unit,'(A,f11.3,A)') '  Min Compute Time:   ',t_elapsed_min,' sec'
     Write(unit,'(A,f11.3,A)') '  Max Compute Time:   ',t_elapsed_max,' sec'
     Write(unit,'(A,f7.2,A)') '  Spin Fraction (approximate): ',100._dp * (t_process - Real(n_img,dp)*t_elapsed_min) / t_process,'%'
-    stat = HOSTNAM(hostname)
-    If (stat .NE. 0) hostname = 'UNKMOWN'
+    hostname = Get_Host_Name()
     If (n_img .GT. 1) Then
         Write(unit,'(3A,I4,A)') '  Host: ',Trim(hostname),', ',n_img,' coarray images'
     Else
