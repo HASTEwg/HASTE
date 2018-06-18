2f6b6b07b00ae42626bb309738349ba46054247e
HATSwg <39804032+HATSwg@users.noreply.github.com>
2018-06-18 09:51:40 -0400

    Update Kinds.f90

--- source/common/Kinds.f90
+++ source/common/Kinds.f90
@@ -7,8 +7,8 @@ Module Kinds
     Integer, Parameter :: dp = Selected_Real_Kind(p=15, r=307)  !double precision
     Integer, Parameter :: qp = Selected_Real_Kind(p=33, r=4931)  !quadruple precision
 
-    Integer, Parameter :: i2 = Selected_Int_Kind(4)  !+/- 10**4
-    Integer, Parameter :: i4 = Selected_Int_Kind(8)  !+/- 10**8
-    Integer, Parameter :: i8 = Selected_Int_Kind(18)  !+/- 10**18
+    Integer, Parameter :: i2 = Selected_Int_Kind(3)  !short-short, +/- 10**4
+    Integer, Parameter :: i4 = Selected_Int_Kind(8)  !short, +/- 10**8
+    Integer, Parameter :: i8 = Selected_Int_Kind(14)  !long, +/- 10**18
     
 End Module Kinds
