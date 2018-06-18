9d8c6282a50a8bdaa63eb07c8613d5304faeb419
HATSwg <39804032+HATSwg@users.noreply.github.com>
2018-06-18 13:13:07 -0400

    Update PRNGs.f90

--- source/common/PRNGs.f90
+++ source/common/PRNGs.f90
@@ -1,5 +1,60 @@
 Module PRNGs
+!-------------------------------------------------------------------------------
+!   A C-program for MT19937: Integer and real number versions
+!      Coded by Takuji Nishimura, considering the suggestions by
+!       Topher Cooper and Marc Rieffel in July-Aug. 1997.
+!       Copyright (C) 1997 Makoto Matsumoto and Takuji Nishimura.
 
+!   A C-program for MT19937-64: 64-bit version of Mersenne Twister PRNG
+!       Coded by Takuji Nishimura and Makoto Matsumoto.
+!       Copyright (C) 2004, Makoto Matsumoto and Takuji Nishimura.
+!
+!   Redistribution and use in source and binary forms, with or without
+!   modification, are permitted provided that the following conditions
+!   are met:
+!
+!     1. Redistributions of source code must retain the above copyright
+!        notice, this list of conditions and the following disclaimer.
+!
+!     2. Redistributions in binary form must reproduce the above copyright
+!        notice, this list of conditions and the following disclaimer in the
+!        documentation and/or other materials provided with the distribution.
+!
+!     3. The names of its contributors may not be used to endorse or promote 
+!        products derived from this software without specific prior written 
+!        permission.
+!
+!   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+!   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+!   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+!   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+!   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+!   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+!   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+!   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+!   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+!   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+!   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+!-------------------------------------------------------------------------------
+!   Fortran Translation of MT19937
+!
+!   Original Fortran translation by Hiroshi Takano.  Jan. 13, 1999.
+!
+!   Fortran translation rewritten as an f90 module by Richard Woloshyn,
+!   (rwww@triumf.ca). June 30, 1999
+!
+!   Revised Fortran translation by Whitman Dailey.  Jun 02, 2018.
+!   Air Force Institute of Technology, Department of Engineering Physics
+!-------------------------------------------------------------------------------
+!   Fortran Translation of MT19937-64
+!
+!   Original Fortran translation by Rﾃｩmi Piatek.  Nov. 04, 2016.
+!   The University of Copenhagen, Department of Economics
+!   email: {first}.{last}@econ.ku.dk
+!
+!   Revised Fortran translation by Whitman Dailey.  Jun 02, 2018.
+!   Air Force Institute of Technology, Department of Engineering Physics
+!-------------------------------------------------------------------------------
     Use Kinds, Only: i4
     Use Kinds, Only: i8
     Implicit None
